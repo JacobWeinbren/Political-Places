d3c78aea182480cf882010e7975d0bf291c4ac9a
Jacob Weinbren <jacobweinbren@gmail.com>
2021-07-04 06:14:46 +0100

    Added in necessary prerequisites

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-import requests, os
-from flask import Flask, render_template
-from dotenv import load_dotenv
+import requests, os, ast
 
+from flask import Flask, render_template
+from flask_minify import minify
+
+from dotenv import load_dotenv
+from sassutils.wsgi import SassMiddleware
+
+#Sets env variables
 load_dotenv()
 clientid  = os.getenv("clientid")
 clientsecret = os.getenv("clientsecret")
 
 app = Flask(__name__)
- # pip install requests
+
+#Minifies output
+minify(app=app, html=True, js=True, cssless=True)
+
+#Handles sass
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'app': {
+        'sass_path': 'static/sass',
+        'css_path': 'static/css',
+        'strip_extension': True,
+    },
+})
+
+#Gets arcgis token
 def get_token():
     params = {
         'client_id': clientid,
@@ -21,7 +39,12 @@ def get_token():
     token = response["access_token"]
     return token
 
+#Main page
 @app.route('/')
 def index():
     token = get_token()
-    return render_template('hello.html', token=token)
+    return render_template('index.html', token=token)
+
+#https encryption
+if __name__ == "__main__":
+    app.run()
