783668bda1ee535151787dc45d781d1bf4841629
Jacob Weinbren <jacobweinbren@gmail.com>
2021-07-04 04:44:28 +0100

    Removed main.js aspects

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from "rollup-plugin-terser";
 import scss from 'rollup-plugin-scss'
-import dotenv from "rollup-plugin-dotenv"
+import { babel } from '@rollup/plugin-babel';
 
 export default {
     input: 'src/scripts/main.js',
@@ -17,6 +17,8 @@ export default {
         commonjs(),
         nodeResolve(),
         terser(),
-        dotenv()
+        babel({
+            exclude: 'node_modules/**'
+        })
     ],
 };
