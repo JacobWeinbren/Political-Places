82afb506a07f695e9e30886edd3c12ab00c47ee9
JacobWeinbren <jacobweinbren@gmail.com>
2021-07-09 00:54:08 +0000

    Deploying to gh-pages from @ JacobWeinbren/Political-Places@ba119796e151a58c93a4cb09c0a507f5b902c936 🚀

---
+++ mk/slider/chunks/BitmapContainer-3519de61.js
@@ -0,0 +1,1 @@
+import{o as e,g as r}from"./WGLContainer-98ad1355.js";import{I as s}from"./Utils-699ce6bc.js";class t extends e{get requiresDedicatedFBO(){return this.children.some((e=>"additive"===e.blendFunction))}prepareRenderPasses(e){const t=e.registerRenderPass({name:"bitmap",brushes:[r.bitmap],target:()=>this.children,drawPhase:s.MAP});return[...super.prepareRenderPasses(e),t]}}export{t};
---
+++ mk/slider/chunks/geometryEngineWorker-facf9ee6.js
@@ -0,0 +1,1 @@
+import{k as e}from"./geometryEngineJSON-131f6730.js";import"./geometryEngineBase-c75b158f.js";import"./json-38690ed0.js";function r(r){return(0,e[r.operation])(...r.parameters)}export{r as executeGEOperation};
