f39e72fee0679aef0bf637cfd8c80605a49d9d62
JacobWeinbren <jacobweinbren@gmail.com>
2021-07-31 10:27:33 +0000

    Deploying to gh-pages from @ JacobWeinbren/Political-Places@4fa6b51ac59fa37c6cca3174bfeaf07e159b784b 🚀

--- mk/classification/chunks/BitmapContainer-4b1cfc20.js
+++
@@ -1,1 +0,0 @@
-import{o as e,g as r}from"./WGLContainer-161f7a04.js";import{I as s}from"./Utils-0f3fb6bb.js";class t extends e{get requiresDedicatedFBO(){return this.children.some((e=>"additive"===e.blendFunction))}prepareRenderPasses(e){const t=e.registerRenderPass({name:"bitmap",brushes:[r.bitmap],target:()=>this.children,drawPhase:s.MAP});return[...super.prepareRenderPasses(e),t]}}export{t};
---
+++ mk/mob_dep/chunks/BitmapContainer-8af3318d.js
@@ -0,0 +1,1 @@
+import{o as e,g as r}from"./WGLContainer-f14d11d0.js";import{I as s}from"./Utils-e7ce534a.js";class t extends e{get requiresDedicatedFBO(){return this.children.some((e=>"additive"===e.blendFunction))}prepareRenderPasses(e){const t=e.registerRenderPass({name:"bitmap",brushes:[r.bitmap],target:()=>this.children,drawPhase:s.MAP});return[...super.prepareRenderPasses(e),t]}}export{t};
